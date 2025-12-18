e3e6dd48563585daa5f03578cbd7bc8c8313f86d
Don Williams <don.e.williams@gmail.com>
2025-12-18 00:53:53 -0500

    Enhancing diagnostics
     On branch ddubs
     Your branch is up to date with 'origin/ddubs'.
    
     Changes to be committed:
    	modified:   lua/utils/diagnostics.lua

--- nvim/lua/utils/diagnostics.lua
+++ nvim/lua/utils/diagnostics.lua
@@ -9,6 +9,7 @@ local diagnostic_signs = {
 
 M.setup = function()
   vim.diagnostic.config({
+    -- Show signs in the gutter
     signs = {
       text = {
         [vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
@@ -16,7 +17,28 @@ M.setup = function()
         [vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
         [vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
       },
+      numhl = {
+        [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+        [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+        [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+        [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+      },
+      linehl = false,
     },
+    -- Show virtual text next to error
+    virtual_text = {
+      severity = {
+        min = vim.diagnostic.severity.WARN,  -- Show WARN and ERROR
+      },
+    },
+    -- Underline errors
+    underline = true,
+    -- Show diagnostic in floating window on hover
+    float = {
+      source = "always",
+    },
+    severity_sort = true,
+    update_in_insert = false,
   })
 end
 
