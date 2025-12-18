1250388245792406bdb2a8d482ea8b0436b9205f
Don Williams <don.e.williams@gmail.com>
2025-12-18 03:16:51 -0500

    markdown preview failed build, mod'd build order
     On branch ddubs
     Your branch is up to date with 'origin/ddubs'.
    
     Changes to be committed:
    	modified:   nvim/lua/plugins/markdown-preview.lua

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -7,7 +7,6 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = "cd app && npm install",
   init = function()
     vim.g.mkdp_auto_start = 0
     vim.g.mkdp_auto_close = 0
