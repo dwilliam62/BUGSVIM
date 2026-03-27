686fc749111aebcd0ee9a0e3635e4346a270fcfa
Don Williams <don.e.williams@gmail.com>
2026-03-26 22:19:56 -0400

    Disabled restore to last line on re-open PITA
     On branch ddubs
     Your branch is up to date with 'origin/ddubs'.
    
     Changes to be committed:
    	modified:   nvim/lua/config/autocmds.lua

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,18 +12,6 @@ vim.api.nvim_create_autocmd('User', {
   end,
 })
 
--- Restore last cursor position when reopening a file
-local last_cursor_group = vim.api.nvim_create_augroup('LastCursorGroup', {})
-vim.api.nvim_create_autocmd('BufReadPost', {
-  group = last_cursor_group,
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
 
 -- Highlight the yanked text for 200ms
 local highlight_yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
